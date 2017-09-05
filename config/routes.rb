b8a15e1d4de0bbeee63ab5b03edf1bcfe3f246ea
Kenia Guimaraes <keniiarruda@gmail.com>
2017-09-05 02:34:10 +0000

    Inclusão de Autenticação do sistema

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,17 @@
 Rails.application.routes.draw do
+  devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 #  resources :application
-  mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
+  mount RailsAdmin::Engine => '/', as: 'rails_admin'
+
+
+    
+  devise_scope :user do
+    get '/login' => 'devise/sessions#new'
+    get '/logout' => 'devise/sessions#destroy'
+    get '/perfil' => 'devise/registrations#edit'
+    get '/novo' => 'devise/registrations#new'
+    get '/user' => 'user#index'
+  end
+  
 end
