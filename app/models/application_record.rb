a9eea3f41c1883fef7a0ca637bc122f321f5195c
Kenia Guimaraes <keniiarruda@gmail.com>
2017-09-05 01:10:10 +0000

    Ajustando modulo de notas

--- app/models/application_record.rb
+++ app/models/application_record.rb
@@ -1,7 +1,4 @@
 class ApplicationRecord < ActiveRecord::Base
-  self.abstract_class = true
-  
-      belongs_to:funcionario
-    belongs_to:tiponotas
-    belongs_to:tipoassunto
+ self.abstract_class = true
+
 end
--- config/initializers/rails_admin.rb
+++ config/initializers/rails_admin.rb
@@ -27,8 +27,8 @@ RailsAdmin.config do |config|
 
   config.main_app_name = ["Controle de Notas   ", "       "]
 
-   config.navigation_static_label = "Lins Úteis"
-   config.navigation_static_links = {
+   config.navigation_static_label = "Links Úteis"
+    config.navigation_static_links = {
    'GitHub' => 'http://github.com/keniaguimaraes' 
   }
  
@@ -47,7 +47,19 @@ RailsAdmin.config do |config|
         label "Data Admissão"
       end 
     end 
-
+    
+   edit do
+      configure :notas do
+        hide
+      end
+      field  :nome do
+        label "Nome"
+      end
+      field  :data_admissao do
+        label "Data Admissão"
+      end 
+    end 
+    
     list do
       field  :nome do
         label "Nome"
@@ -57,19 +69,9 @@ RailsAdmin.config do |config|
       end  
     end 
     
-    edit do
-     configure :notas do
-        hide
-      end
-      field  :nome do
-        label "Nome"
-      end
-      field  :data_admissao do
-        label "Data Admissão"
-      end 
-     
-    end 
- end    
+ end
+ 
+ 
      
  
   
@@ -108,7 +110,7 @@ RailsAdmin.config do |config|
  config.model Tipoassunto do
     navigation_icon 'fa fa-comment-o'  #inclui icone
     weight -3                   #configura a ordem no menu lateral
-   
+ 
      create do
       configure :notas do
         hide
@@ -139,41 +141,165 @@ RailsAdmin.config do |config|
  config.model Nota do
    navigation_icon 'fa fa-sticky-note-o'  #inclui icone
    weight -1                   #configura a ordem no menu lateral
-   
-      create do 
+
+    create do 
     
-          field :funcionario_id , :enum do
+         field :funcionario_id , :enum do
             enum do
-              Funcionario.all.map{ |a| [ a.nome, a.id ] }
+              Funcionario.all.map{ |a| [a.nome,a.id] }
             end
+            label "Funcionário"
            end
-          
-          field :tiponota_id , :enum do
+
+          field :tiponota_id, :enum do
             enum do
-              Tiponota.all.map{ |c| [ c.descricao, c.id ] }
+              Tiponota.all.map{ |a| [a.descricao,a.id] }
             end
-          end
-          
+             label "Nota"
+           end
+         
           field:data do
-                label "data"
+                label "Data"
           end  
           
-          field :tipoassunto , :enum do
+          field :tipoassunto_id , :enum do
             enum do
-             Tipoassunto.all.map{ |b| [ b.descricao, b.id ] }
+              Tipoassunto.all.map{ |a| [a.descricao,a.id] }
             end
+            label  "Tipo Assunto"
+           end
+           
+          field:destinatario do
+                 label "Destinatário"
           end  
+          
           field:assunto do
                  label "Assunto"
           end  
           field:descricao do
                  label "Descrição"
           end  
-    
-      
       end
+   
+   
+     list do 
+    
+           field :funcionario_id , :enum do
+            enum do
+              Funcionario.all.map{ |a| [a.nome,a.id] }
+            end
+            label "Funcionário"
+           end
+
+      
+          field:data do
+                label "Data"
+          end  
+          
+          field :tipoassunto_id , :enum do
+            enum do
+              Tipoassunto.all.map{ |a| [a.descricao,a.id] }
+            end
+            label  "Tipo Assunto"
+           end
+           
+          field:destinatario do
+                 label "Destinatário"
+          end  
+          
+          field:assunto do
+                 label "Assunto"
+          end  
+      end
+      
+      
+      edit do 
+    
+            field :funcionario_id , :enum do
+            enum do
+              Funcionario.all.map{ |a| [a.nome,a.id] }
+            end
+            label "Funcionário"
+           end
+
+          field :tiponota_id, :enum do
+            enum do
+              Tiponota.all.map{ |a| [a.descricao,a.id] }
+            end
+             label "Nota"
+           end
+         
+          
+          field:data do
+                label "Data"
+          end  
+          
+          field :tipoassunto_id , :enum do
+            enum do
+              Tipoassunto.all.map{ |a| [a.descricao,a.id] }
+            end
+            label  "Tipo Assunto"
+           end
+           
+          field:destinatario do
+                 label "Destinatário"
+          end  
+          
+          field:assunto do
+                 label "Assunto"
+          end  
+          field:descricao do
+                 label "Descrição"
+          end  
+      end
+      
+     show do
+       
+          field :funcionario_id , :enum do
+            enum do
+              Funcionario.all.map{ |a| [a.nome,a.id] }
+            end
+            label "Funcionário"
+           end
+
+          field :tiponota_id, :enum do
+            enum do
+              Tiponota.all.map{ |a| [a.descricao,a.id] }
+            end
+             label "Nota"
+           end
+         
+          
+          field:data do
+                label "Data"
+          end  
+          
+          field :tipoassunto_id , :enum do
+            enum do
+              Tipoassunto.all.map{ |a| [a.descricao,a.id] }
+            end
+            label  "Tipo Assunto"
+           end
+           
+          field:destinatario do
+                 label "Destinatário"
+          end  
+          
+          field:assunto do
+                 label "Assunto"
+          end  
+          field:descricao do
+                 label "Descrição"
+          end  
+       
+     end   
+      
+      
+   
 end
-  
+
+   
+
 
  
   config.actions do
